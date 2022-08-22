7dbb79ff7aaeb1a3b88e12acff19c513b68e7d08
qunash <anzoria@gmail.com>
2022-08-22 14:58:19 +0300

    ~translate.py

--- translate.py
+++ translate.py
@@ -1,13 +1,16 @@
-def translate(text, lang_id="zu_Latn"):
+def translate(text, src_lang="zu_Latn", tgt_lang="rus_Cyrl"):
+
     from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-    tokenizer = AutoTokenizer.from_pretrained("./models/nllb_ru_kbd_44K")
-    model = AutoModelForSeq2SeqLM.from_pretrained("./models/nllb_ru_kbd_44K")
+    model_path = "anzorq/nllb_ru-kbd_44K_"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path, src_lang=src_lang)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
     inputs = tokenizer(text, return_tensors="pt")
 
     translated_tokens = model.generate(
-        **inputs, forced_bos_token_id=tokenizer.lang_code_to_id[lang_id], num_beams=4, num_return_sequences=4
+        **inputs, forced_bos_token_id=tokenizer.lang_code_to_id[tgt_lang], num_beams=4, num_return_sequences=4
     )
 
     for translation in tokenizer.batch_decode(translated_tokens, skip_special_tokens=True):
